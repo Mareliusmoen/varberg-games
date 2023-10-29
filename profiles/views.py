65979b94033d2748a6d313a8adc634d163ec5656
Mareliusmoen <mareliusmoen@gmail.com>
2023-10-29 19:03:43 +0100

    add Profiles model, views and url path

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Profile
 
-# Create your views here.
+
+class ProfileList(generic.ListView):
+    model = Profile
+    template_name = 'index.html'
+    context_object_name = 'profiles'
--- varberggames/settings.py
+++ varberggames/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vareberg-games-a44cb52aa87b.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['vareberg-games-a44cb52aa87b.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
