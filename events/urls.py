c2c8a81355c4c7bad2bedcfff675aadfcf31b082
Mareliusmoen <mareliusmoen@gmail.com>
2023-11-01 16:07:49 +0100

    add datetimepicker from jquery to create_event

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('events/', views.event_list, name='events'),
     path('create_event/', views.create_event, name='create_event'),
+    path('events/', views.event_list, name='event_list'),
 ]
