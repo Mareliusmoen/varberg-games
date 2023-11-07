2b564831fb2ac378d3271ff9f93ee33183575470
Mareliusmoen <mareliusmoen@gmail.com>
2023-11-07 16:21:13 +0100

    Styling of the chats.html modal

--- chats/routing.py
+++ chats/routing.py
@@ -1,12 +1,11 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path, re_path
+from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'wss/chat/<str:user_id>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/chat/<str:user_id>/', consumers.ChatConsumer.as_asgi()),
 ]
+
 application = ProtocolTypeRouter({
-    'websocket': URLRouter([
-        path('wss/chat/<str:user_id>/', consumers.ChatConsumer.as_asgi()),
-    ])
+    'websocket': URLRouter(websocket_urlpatterns),
 })
