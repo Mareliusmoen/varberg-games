5bbbf69adac6eb2af00ef2fc96ab119ca2820020
Mareliusmoen <mareliusmoen@gmail.com>
2023-11-15 11:44:02 +0100

    add edit functionality for product-creator marketplace

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create_product', views.create_product, name='create_product'),
     path('your_product_list', views.your_product_list, name='your_product_list'),
     path('product/<int:product_id>/delete/', views.delete_product, name='delete_product'),
-
+    path('product/<int:product_id>/edit/', views.edit_product, name='edit_product'),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -46,6 +46,47 @@ def create_product(request):
         form = ProductForm()
     return render(request, 'marketplace/create_product.html', {'form': form})
 
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            product = form.save(commit=False)
+            
+            # Get the category instance based on the selected category string
+            category_str = form.cleaned_data['category']
+            category_instance = Category.objects.get(name=category_str)
+            
+            # Assign the category instance to the product
+            product.category = category_instance
+
+            # Set the seller to the current user
+            product.seller = request.user
+
+            # Add card_name handling here
+            card_name = form.cleaned_data['card_name']
+            if card_name:
+                # Handle card search and API request here
+                api_url = 'https://api.magicthegathering.io/v1/cards'
+                params = {'name': card_name}
+                response = requests.get(api_url, params=params)
+                if response.status_code == 200:
+                    card_data = response.json()
+                    if 'cards' in card_data and len(card_data['cards']) > 0:
+                        # Fetch the first card's data
+                        card = card_data['cards'][0]
+                        # Set the card data in the product
+                        product.card_name = card['name']
+                        product.card_image_url = card['imageUrl']
+            
+            # Save the product after card handling
+            product.save()
+            messages.success(request, 'Your item was updated successfully.')
+            return redirect('product_list')  # Redirect to product_list after successfully saving the product
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'marketplace/edit_product.html', {'form': form, 'product': product})
+
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'marketplace/product_list.html', {'products': products})
